8238d663903f1a386b6ba1ebfec7ea2921eb6eae
turekjesse <turekjesse@gmail.com>
2021-07-30 17:07:13 -0400

    boards rendering from localhost

--- src/components/Boards/Boards.js
+++ src/components/Boards/Boards.js
@@ -1,6 +1,48 @@
+import { useState, useEffect } from 'react'
+import { Header, Grid, Image, Segment } from 'semantic-ui-react'
+
 const Boards = () => {
+
+    const [boards, setBoards] = useState([]);
+
+	const getBoardsIndex = async () => {
+		try {
+			const response = await fetch('http://localhost:8000/boards/');
+			const data = await response.json();
+			console.log(data);
+			setBoards(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		getBoardsIndex();
+	}, []);
+
+	if (!boards.length) {
+		return <p>The quiver is empty!</p>;
+	}
+    
     return (
-        <p>This is boards</p>
+        <main>
+            <Header textAlign="center" as='h1'>Boards</Header>
+            <Grid>
+                {
+                    boards.map((board, idx) => {
+                        return (
+                            <Segment>
+                                <Grid.Column>
+                                    <Image size='medium' src={board.image_url} />
+                                    <Header textAlign="center" as='h3'>{board.model}</Header>
+                                    <Header textAlign="center" as='h5'>{board.brand}</Header>
+                                    
+                                </Grid.Column>
+                            </Segment>
+                        )
+                    })
+                }
+            </Grid>
+        </main>
     )
 }
 
