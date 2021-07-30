096d57f7df04447021bb1da88f53e5298771ed39
turekjesse <turekjesse@gmail.com>
2021-07-30 17:06:41 -0400

    routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {Route} from 'react-router-dom'
 import './App.css';
+import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
+import Boards from './components/Boards/Boards';
+// import BoardDetail from './components/BoardDetail/BoardDetail';
+// import AddBoard from './components/AddBoard/AddBoard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Route path="/" exact render={() => <Home/>}/>
+      <Route path="/boards" render={() => <Boards/>}/>
     </div>
   );
 }
